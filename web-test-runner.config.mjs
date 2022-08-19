aa401e57040ffe38141f25d6ba6ad1056ff92844
Jereme Evans <jereme@jeremeevans.com>
2022-08-19 11:45:48 -0700

    Set up Cypress, added a test

--- web-test-runner.config.mjs
+++
@@ -1,8 +0,0 @@
-import { esbuildPlugin } from "@web/dev-server-esbuild"
-import { chromeLauncher } from "@web/test-runner"
-
-export default {
-  files: ["./src/example-element.tests.ts"],
-  plugins: [esbuildPlugin({ ts: true })],
-  browsers: [chromeLauncher({ launchOptions: { args: ["--no-sandbox"] } })],
-}
